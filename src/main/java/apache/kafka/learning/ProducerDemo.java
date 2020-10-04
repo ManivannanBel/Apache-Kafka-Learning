181ccbb8149e630387a19887e399355adca8fdf7
manivannanbel <manivannan.belfazt@gmail.com>
2020-10-04 07:25:36 -0700

    Simple producer

--- src/main/java/apache/kafka/learning/ProducerDemo.java
+++ src/main/java/apache/kafka/learning/ProducerDemo.java
@@ -1,7 +1,36 @@
 package apache.kafka.learning;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
+        String bootstrapServer = "localhost:9092";
+
+        //Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //Create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //Create Producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "Hello World!");
+
+        //Send data
+        producer.send(record);
+
+        //Flush producer
+        producer.flush();
+        //Flush and close producer
+        producer.close();
+
         System.out.println("Hello");
     }
 }
