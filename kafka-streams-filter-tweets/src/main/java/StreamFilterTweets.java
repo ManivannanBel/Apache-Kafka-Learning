5d7b37513bb23c4f73eb7637c5a16dc2034f2dfb
manivannanbel <manivannan.belfazt@gmail.com>
2020-10-10 19:56:10 +0530

    kafka streams to filter tweets

---
+++ kafka-streams-filter-tweets/src/main/java/StreamFilterTweets.java
@@ -0,0 +1,14 @@
+public class StreamFilterTweets {
+
+    public static void main(String[] args) {
+        //create  properties
+
+        //create topology
+
+        //build the topology
+
+        //Start the streams application
+        
+    }
+
+}
