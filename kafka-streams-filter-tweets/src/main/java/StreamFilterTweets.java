fa2656567eb05ac1fc84306c3c2753f31f73425e
manivannanbel <manivannan.belfazt@gmail.com>
2020-10-10 20:37:46 +0530

    kafka streams to filter tweets with users having followers greater than 10000

--- kafka-streams-filter-tweets/src/main/java/StreamFilterTweets.java
+++ kafka-streams-filter-tweets/src/main/java/StreamFilterTweets.java
@@ -1,14 +1,57 @@
+import com.google.gson.JsonParser;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
 public class StreamFilterTweets {
 
     public static void main(String[] args) {
         //create  properties
+        Properties properties = new Properties();
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "demo-kafka-streams");
+        properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+        properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
 
         //create topology
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        //input topic
+        KStream<String, String> inputTopic = streamsBuilder.stream("twitter_topic");
+        KStream<String, String> filteredTopic = inputTopic.filter(
+                //filter tweets of the users with followers above 10000
+                (key, value) -> extractTwitterUserFollowers(value ) > 10000
+        );
+        filteredTopic.to("important_tweets");
 
         //build the topology
+        KafkaStreams kafkaStreams = new KafkaStreams(
+                streamsBuilder.build(),
+                properties
+        );
 
         //Start the streams application
-        
+        kafkaStreams.start();
+    }
+
+    public  static JsonParser jsonParser = new JsonParser();
+    public static Integer extractTwitterUserFollowers(String jsonString){
+        try{
+            Integer followers = jsonParser.parse(jsonString)
+                    .getAsJsonObject()
+                    .get("user")
+                    .getAsJsonObject()
+                    .get("followers_count")
+                    .getAsInt();
+            return followers;
+        }catch (NullPointerException e) {
+            return 0;
+        }
     }
 
 }
